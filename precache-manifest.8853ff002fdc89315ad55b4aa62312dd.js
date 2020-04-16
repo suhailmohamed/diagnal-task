b3d7b25d22e2f332481edd50e3b9717ad37bf85c
suhailmohamed <suhailmohamed333@gmail.com>
2020-04-16 14:20:44 +0530

    Updates

---
+++ precache-manifest.8853ff002fdc89315ad55b4aa62312dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d9fe6f55180e378f53e",
+    "url": "/diagnal-task/static/css/main.5eda3535.chunk.css"
+  },
+  {
+    "revision": "9d9fe6f55180e378f53e",
+    "url": "/diagnal-task/static/js/main.9d9fe6f5.chunk.js"
+  },
+  {
+    "revision": "a988b594e65522b79bd2",
+    "url": "/diagnal-task/static/js/runtime~main.a988b594.js"
+  },
+  {
+    "revision": "b071f3dba7afb4a0ec27",
+    "url": "/diagnal-task/static/js/2.b071f3db.chunk.js"
+  },
+  {
+    "revision": "a1f3dc123a688c690289b7cd30942f97",
+    "url": "/diagnal-task/index.html"
+  }
+];
